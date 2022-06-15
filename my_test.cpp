df6746008a68c21e1f77f745fdee99288afba734
Mike Alexandrovsky <mike.alexandrovsky@gmail.com>
2022-06-15 13:52:51 +0300

    Test auto created files

---
+++ my_test.cpp
@@ -0,0 +1,23 @@
+//PRIVATE TESTING
+
+#include "Players/Player.h"
+#include "Cards/Card.h"
+#include "Mtmchkin.h"
+#include "Cards/Vampire.h"
+#include "Cards/Barfight.h"
+#include "Cards/Dragon.h"
+#include "Cards/Fairy.h"
+#include "Cards/Goblin.h"
+#include "Cards/Merchant.h"
+#include "Cards/Pitfall.h"
+#include "Cards/Treasure.h"
+#include "Players/Rogue.h"
+#include "Players/Wizard.h"
+#include "Players/Fighter.h"
+#include "Exception.h"
+
+int main() {
+    Mtmchkin game("deck.txt");
+
+    return 0;
+}
